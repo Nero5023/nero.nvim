4a5237e355f4b43ad6bbef17399312a125cad5c4
Chenhao Zuo <nero@meta.com>
2024-07-21 18:03:22 -0700

    Add search in directory feature for telescope

--- lua/custom/telescope.lua
+++ lua/custom/telescope.lua
@@ -50,12 +50,108 @@ pcall(require('telescope').load_extension, 'ui-select')
 
 -- See `:help telescope.builtin`
 local builtin = require 'telescope.builtin'
+
+--#region set up search in path logic
+---@return string|nil
+local function get_search_path()
+  local path = vim.g.search_path
+  if path == nil then
+    -- when not set, we just use the project root as the search path
+    path = vim.fn.getcwd()
+  end
+  return path
+end
+
+---@param path string|nil
+local function set_search_path(path)
+  if path == nil then
+    path = vim.fn.getcwd()
+  end
+  vim.g.search_path = path
+  vim.print('set search path: ' .. vim.g.search_path)
+end
+
+---Get the path that oil interface represent if in oil
+---@return nil|string
+local function get_search_path_in_oil()
+  local oil = require 'oil'
+  local oil_current_dir = oil.get_current_dir()
+  if oil_current_dir == nil then
+    -- not in oil current dir
+    -- in this case, we just return nil
+    return nil
+  else
+    -- in oil interface
+    local cursor_oil_entry = oil.get_cursor_entry()
+    if cursor_oil_entry == nil then
+      return oil_current_dir
+    elseif cursor_oil_entry.type == 'file' then
+      return oil_current_dir
+    elseif cursor_oil_entry.type == 'directory' then
+      return vim.fs.joinpath(oil_current_dir, cursor_oil_entry.name)
+    end
+  end
+end
+
+---Check if is in oil interface
+---@return boolean
+local function is_in_oil()
+  local oil = require 'oil'
+  return oil.get_current_dir() ~= nil
+end
+
+---Use in telescope search, if it is in oil, it will set the path in oil as the global search_path
+---and use that path for searhing. Else jsut return the global search_path
+---@return string|nil
+local function setup_telescope_search_path()
+  local path = nil
+  if is_in_oil() then
+    -- if in oil, we just set the search path base in oil interface
+    path = get_search_path_in_oil()
+    set_search_path(path)
+  else
+    -- use the glocal search_path
+    path = get_search_path()
+  end
+  vim.print('Searching in path: ' .. path)
+  return path
+end
+
+local function find_files()
+  local path = setup_telescope_search_path()
+  local opts = {
+    cwd = path,
+  }
+  builtin.find_files(opts)
+end
+
+local function grep_string()
+  local path = setup_telescope_search_path()
+  local opts = {
+    cwd = path,
+  }
+  builtin.grep_string(opts)
+end
+
+local function live_grep()
+  local path = setup_telescope_search_path()
+  local opts = {
+    cwd = path,
+  }
+  builtin.live_grep(opts)
+end
+--#endregion
+
+vim.keymap.set('n', '<C-`>', function()
+  set_search_path(vim.fn.getcwd())
+end, { desc = 'Reset telescope search path to project root (` -> ~ -> project root)' })
+
 vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
 vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+vim.keymap.set('n', '<leader>sf', find_files, { desc = '[S]earch [F]iles' })
 vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+vim.keymap.set('n', '<leader>sw', grep_string, { desc = '[S]earch current [W]ord' })
+vim.keymap.set('n', '<leader>sg', live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
 vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
 vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
