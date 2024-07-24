7f7d9b94b5536510736c61e23ca43ecc90c8576e
nero5023 <nero5023@gmail.com>
2024-07-23 23:15:35 -0700

    add desc keymap for harpoon

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -6,49 +6,27 @@ return {
     local harpoon = require 'harpoon'
     harpoon:setup()
 
-    vim.keymap.set('n', '<leader>ha', function()
+    local set = vim.keymap.set
+
+    set('n', '<leader>ha', function()
       harpoon:list():add()
-    end)
-    vim.keymap.set('n', '<leader>hl', function()
+    end, { desc = '[a]dd to harpoon list' })
+    set('n', '<leader>hl', function()
       harpoon.ui:toggle_quick_menu(harpoon:list())
-    end)
+    end, { desc = '[l]list all items in harpoon' })
 
     -- Toggle previous & next buffers stored within Harpoon list
-    vim.keymap.set('n', '<C-S-P>', function()
+    set('n', '<C-S-P>', function()
       harpoon:list():prev()
-    end)
-    vim.keymap.set('n', '<C-S-N>', function()
+    end, { desc = 'Go previous item in harpoon' })
+    set('n', '<C-S-N>', function()
       harpoon:list():next()
-    end)
+    end, { desc = 'Go next item in harpoon' })
 
-    --#region set access to list by index
-    vim.keymap.set('n', '<leader>h1', function()
-      harpoon:list():select(1)
-    end)
-    vim.keymap.set('n', '<leader>h2', function()
-      harpoon:list():select(2)
-    end)
-    vim.keymap.set('n', '<leader>h3', function()
-      harpoon:list():select(3)
-    end)
-    vim.keymap.set('n', '<leader>h4', function()
-      harpoon:list():select(4)
-    end)
-    vim.keymap.set('n', '<leader>h5', function()
-      harpoon:list():select(5)
-    end)
-    vim.keymap.set('n', '<leader>h6', function()
-      harpoon:list():select(6)
-    end)
-    vim.keymap.set('n', '<leader>h7', function()
-      harpoon:list():select(7)
-    end)
-    vim.keymap.set('n', '<leader>h8', function()
-      harpoon:list():select(8)
-    end)
-    vim.keymap.set('n', '<leader>h9', function()
-      harpoon:list():select(9)
-    end)
-    --#endregion
+    for i = 1, 5 do
+      set('n', '<leader>h' .. i, function()
+        harpoon:list():select(i)
+      end, { desc = 'Go to ' .. i .. 'th item in harpoon' })
+    end
   end,
 }
