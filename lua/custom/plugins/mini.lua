eec01d5dbea3fa70c2c3ce35f50ee5e8ce722f0d
nero <nero@gmail.com>
2024-07-27 20:03:07 -0700

    update mini surroundn

--- lua/custom/plugins/mini.lua
+++ lua/custom/plugins/mini.lua
@@ -12,9 +12,15 @@ return { -- Collection of various small independent plugins/modules
     -- Add/delete/replace surroundings (brackets, quotes, etc.)
     --
     -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-    -- - sd'   - [S]urround [D]elete [']quotes
+    -- - ds'   - [D]elete [S]urround[']quotes
     -- - sr)'  - [S]urround [R]eplace [)] [']
-    require('mini.surround').setup()
+    -- - cs)'  - [C]hange [S]urround  [)] [']
+    require('mini.surround').setup {
+      mappings = {
+        delete = 'ds',
+        replacee = 'cs',
+      },
+    }
 
     -- Simple and easy statusline.
     --  You could remove this setup call if you don't like it,
