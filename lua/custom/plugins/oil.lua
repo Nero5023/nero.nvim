23ff233e11d0d5bb5bf0fd044d7f1a1a48ff3797
Chenhao Zuo <nero@meta.com>
2024-08-02 19:35:55 -0700

    fix oil horizontal split issue

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -11,8 +11,8 @@ return {
         ['<C-h>'] = false,
         -- ['<M-h>'] = 'actions.select_split',
         -- ['<C-s>'] = false,
-        ['<C-v>'] = { 'actions.select', opts = { vertical = true }, desc = 'Open the entry in a vertical split' },
-        ['<C-s>'] = { 'actions.select', opts = { horizontal = true }, desc = 'Open the entry in a horizontal split' },
+        ['<C-v>'] = { 'actions.select', opts = { vertical = true, horizontal = false }, desc = 'Open the entry in a vertical split' },
+        ['<C-s>'] = { 'actions.select', opts = { horizontal = true, vertical = false }, desc = 'Open the entry in a horizontal split' },
       },
       view_options = {
         show_hidden = true,
@@ -22,7 +22,7 @@ return {
     -- Open parent directory in current window
     vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
 
-    -- Open parent directory in floating window
+    -- Open parent directory in floating windowoil
     vim.keymap.set('n', '<space>-', require('oil').toggle_float)
   end,
 }
