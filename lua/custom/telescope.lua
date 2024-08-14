49a286535d32ab3bbf96d8bdfa108046b94acc0d
nero5023 <nero5023@gmail.com>
2024-08-14 12:55:36 -0700

    update telescope

--- lua/custom/telescope.lua
+++ lua/custom/telescope.lua
@@ -201,7 +201,8 @@ vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iag
 vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
 vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
 vim.keymap.set('n', '<leader>b', builtin.buffers, { desc = '[ ] Find existing buffers' })
-vim.keymap.set('n', '<leader>sb', builtin.buffers, { desc = '[ ] [S]earch existing [B]bffers' })
+vim.keymap.set('n', '<leader>sb', builtin.buffers, { desc = '[S]earch existing [B]bffers' })
+vim.keymap.set('n', '<leader>sq', builtin.quickfix, { desc = '[S]earch items in [Q]uickfix list' })
 
 -- Slightly advanced example of overriding default behavior and theme
 vim.keymap.set('n', '<leader>/', function()
