98430564f4ac6ab31d9ec9182188ad526f465eed
nero5023 <nero5023@gmail.com>
2025-03-05 17:53:57 -0800

    update highlight indent line

--- lua/kickstart/plugins/indent_line.lua
+++ lua/kickstart/plugins/indent_line.lua
@@ -9,7 +9,7 @@ return {
         char = '│',
         tab_char = '│',
       },
-      scope = { show_start = false, show_end = false },
+      scope = { show_start = false, show_end = false, char = '┃' },
       exclude = {
         filetypes = {
           'Trouble',
