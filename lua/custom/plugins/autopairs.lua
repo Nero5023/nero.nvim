3db9c0411ed1e92d124f6b6c261640bca36385c2
nero5023 <nero5023@gmail.com>
2025-03-22 00:49:14 -0700

    Add blink support, using env variable to triiger for now

--- lua/custom/plugins/autopairs.lua
+++ lua/custom/plugins/autopairs.lua
@@ -1,7 +1,7 @@
 -- autopairs
 -- https://github.com/windwp/nvim-autopairs
 
-return {
+local config = {
   'windwp/nvim-autopairs',
   event = 'InsertEnter',
   -- Optional dependency
@@ -18,3 +18,27 @@ return {
     cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
   end,
 }
+
+local config_for_blink = {
+  'windwp/nvim-autopairs',
+  event = 'InsertEnter',
+  -- Optional dependency
+  -- dependencies = { 'hrsh7th/nvim-cmp' },
+  config = function()
+    -- hock on cmp to add close pair to it if possible
+    require('nvim-autopairs').setup {
+      -- set up fast wrap as default, press <M-e> to fast wrap
+      fast_wrap = {},
+    }
+    -- If you want to automatically add `(` after selecting a function or method
+    -- local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
+    -- local cmp = require 'cmp'
+    -- cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+  end,
+}
+
+if os.getenv 'NVIM_BLINK_CMP' == '1' then
+  return config_for_blink
+else
+  return config
+end
