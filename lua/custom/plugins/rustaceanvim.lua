75bba8dd1b7296c2d340f05d6902ba3d1729e844
Chenhao Zuo <nero@meta.com>
2025-05-30 22:33:16 -0700

    update rustaceanvim

--- lua/custom/plugins/rustaceanvim.lua
+++ lua/custom/plugins/rustaceanvim.lua
@@ -15,14 +15,14 @@ return {
       server = {
         -- Set up rust analyzer path
         cmd = function()
-          local mason_registry = require 'mason-registry'
-          local ra_binary = mason_registry.is_installed 'rust-analyzer'
-              -- This may need to be tweaked, depending on the operating system.
-              and mason_registry.get_package('rust-analyzer'):get_install_path() .. '/' .. mason_registry
-                .get_package('rust-analyzer')
-                :get_receipt()
-                :get()['links']['bin']['rust-analyzer']
-            or 'rust-analyzer'
+          local ra_binary = nil
+          if vim.fn.executable 'rust-analyzer' == 1 then
+            ra_binary = 'ra_binary'
+          else
+            local mason_registry = require 'mason-registry'
+            ra_binary = mason_registry.is_installed 'rust-analyzer' and vim.env.MASON .. '/bin/' .. 'rust-analyzer'
+          end
+
           return { ra_binary } -- You can add args to the list, such as '--log-file'
         end,
       },
