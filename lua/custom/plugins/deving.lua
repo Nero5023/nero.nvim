2fb86769b7f7bfb0a01c4ebfefc685db14ccb978
nero5023 <nero5023@gmail.com>
2025-10-24 18:38:52 -0700

    Add conditional loading for local development plugins
      Only load local development plugins (present.nvim, dev-buck.nvim, buck2.nvim)
      when their directories exist, preventing errors when working across different machines.

--- lua/custom/plugins/deving.lua
+++ lua/custom/plugins/deving.lua
@@ -1,5 +1,20 @@
 return {
-  { dir = '~/plugins/present.nvim' },
-  { dir = '~/plugins/dev-buck.nvim' },
-  { dir = '~/plugins/buck2.nvim' },
+  {
+    dir = '~/plugins/present.nvim',
+    cond = function()
+      return vim.fn.isdirectory(vim.fn.expand '~/plugins/present.nvim') == 1
+    end,
+  },
+  {
+    dir = '~/plugins/dev-buck.nvim',
+    cond = function()
+      return vim.fn.isdirectory(vim.fn.expand '~/plugins/dev-buck.nvim') == 1
+    end,
+  },
+  {
+    dir = '~/plugins/buck2.nvim',
+    cond = function()
+      return vim.fn.isdirectory(vim.fn.expand '~/plugins/buck2.nvim') == 1
+    end,
+  },
 }
